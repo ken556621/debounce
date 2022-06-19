773dbc10c2b4f9180fe8b9b4d93dae672990985c
Ken <ken556621@gmail.com>
2022-06-19 16:30:38 +0800

    Add debounce function

---
+++ debounce.js
@@ -0,0 +1,11 @@
+function debounce(func, timeout = 3000) {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+    
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
